59a55d0ff66b11c1e27b177d115cf5f36369e740
Séb <seb_perso@hotmail.fr>
2019-02-09 15:14:26 +0100

    Ajout de la classe Constantes

---
+++ app/src/main/java/com/application/sb/moodtacker/tool/Constantes.java
@@ -0,0 +1,26 @@
+package com.application.sb.moodtacker.tool;
+
+import com.application.sb.moodtacker.R;
+
+public class Constantes {
+
+    // Number of history
+    final public static int HISTORY_NB = 7;
+
+    // Number of Mood
+    final public static int MOOD_NB = 5;
+
+    // Date tab
+    final public static String DATE_TAB[] = {"One day ago", "Two days ago", "Three days ago", "Four days ago", "Five days ago", "Six days ago", "Seven days ago"};
+
+    // The music tab
+    final public static int MUSIC_TAB[] = {R.raw.very_happy, R.raw.happy, R.raw.normal, R.raw.disapointed, R.raw.sad};
+
+    // The colors tab
+    final public static int COLOR_TAB[] = {R.color.banana_yellow, R.color.light_sage, R.color.cornflower_blue_65, R.color.warm_grey, R.color.faded_red};
+
+    // Shared preferences VALUES
+    final public static String CURRENT_MOOD = "currentMood";
+    final public static String MOOD_ARRAYLIST = "moodArrayList";
+
+}
